ceda9603b262f85742777ea8337f26b338227ace
Mark Dueri <mark.dueri@gmail.com>
2021-12-31 18:24:44 -0700

    Clarification Comments.

--- Question2/Question2/ListSorter.cs
+++ Question2/Question2/ListSorter.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace Question2
 {
     /// <summary>
-    /// ListSorter Class - Has no public methods. Uses a constructor to input the original unsorted list and the class does the work.  
-    /// Uses generics to that any type of list can be passed into the class.
+    /// ListSorter Class - Has no public methods. The client code calls the constructor to input the original unsorted list and the class does the work.  
+    /// I use generics so that any type of list can be passed into the class. In this case, I am testing with ints and strings.
     /// </summary>
     public class ListSorter<T>
     {
@@ -35,7 +35,8 @@ namespace Question2
 
 
         /// <summary>
-        /// This method will count each distinct value in the sorted list.
+        /// This method will count each distinct value in the sorted list and put the results in the ValueInfo class
+        /// It may seem inefficient to create a class for each returned value, I'm assuming a real world situation where return class message objects are common.
         /// </summary>
         private void FillValueInfoList ()
         {
